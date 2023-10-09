b02fd9d4bd5229e2e2afc70bf4bef00a73b12f42
tncsito <eduarosantoyo@gmail.com>
2023-10-09 09:03:43 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_CARS/MVVM_implementacion_CARS/VistaModelo/VMpatron.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_CARS.VistaModelo
+{
+    class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        #endregion
+        #region OBJETOS
+
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
